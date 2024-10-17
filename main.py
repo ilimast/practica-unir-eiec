f1144b0559550d0e467058963c846ef0e54a0cc2
caltamirano-ecotec <137807468+caltamirano-ecotec@users.noreply.github.com>
2024-10-17 15:02:06 -0500

    Funcionalidad para ordenar palabras

--- main.py
+++ main.py
@@ -6,14 +6,15 @@ Organization: UNIR
 import os
 import sys
 
-DEFAULT_FILENAME = "words.txt"
+DEFAULT_FILENAME = "palabras.txt"
 DEFAULT_DUPLICATES = False
+DEFAULT_ORDER = "asc"  # Valor predeterminado para el orden
 
 
 def sort_list(items, ascending=True):
     if not isinstance(items, list):
         raise RuntimeError(f"No puede ordenar {type(items)}")
-
+    
     return sorted(items, reverse=(not ascending))
 
 
@@ -24,24 +25,31 @@ def remove_duplicates_from_list(items):
 if __name__ == "__main__":
     filename = DEFAULT_FILENAME
     remove_duplicates = DEFAULT_DUPLICATES
-    if len(sys.argv) >= 2:
+    order = DEFAULT_ORDER
+
+    if len(sys.argv) >= 3:
         filename = sys.argv[1]
-        if len(sys.argv) == 3:
-            remove_duplicates = sys.argv[2].lower() == "yes"
+        remove_duplicates = sys.argv[2].lower() == "yes"
+        if len(sys.argv) == 4:
+            order = sys.argv[3].lower()
     else:
-        print("Uso: python3 main.py <fichero> [<eliminar_duplicados: yes/no>]")
+        print("Uso: python3 main.py <fichero> <eliminar_duplicados: yes/no> [<orden: asc/desc>]")
         sys.exit(1)
 
     print(f"Se leerán las palabras del fichero {filename}")
     file_path = os.path.join(".", filename)
+
     if os.path.isfile(file_path):
         word_list = []
         with open(file_path, "r") as file:
             for line in file:
                 word_list.append(line.strip())
     else:
-        print(f"El fichero {filename} no existe")
+        print(f"El fichero {filename} no existe, se usarán palabras predeterminadas")
         word_list = ["ravenclaw", "gryffindor", "slytherin", "hufflepuff"]
+
     if remove_duplicates:
         word_list = remove_duplicates_from_list(word_list)
-    print(sort_list(word_list))
+
+    ascending = order == "asc"
+    print(sort_list(word_list, ascending=ascending))
